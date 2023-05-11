f7da19fd5168af400ea43602a2c37c4cb11385f8
manoji <67107548+manoji099@users.noreply.github.com>
2023-05-12 00:11:37 +0530

    the notes app has been updated

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
 
-// App Routes
-router.get('/',mainController.homepage);
-router.get('/about',mainController.about);
+/**
+ * App Routes 
+*/
+router.get('/', mainController.homepage);
+router.get('/about', mainController.about);
 
 module.exports = router;
